66c6a5f093f4a872213d6425e35ae687c785b784
Danke Xie <dxie@sta.samsung.com>
2013-05-21 23:04:02 -0700

    Add Match files

---
+++ jvec/src/edu/scu/llvm/translate/WildcardConstant.java
@@ -0,0 +1,9 @@
+package edu.scu.llvm.translate;
+
+import cn.edu.sjtu.jllvm.VMCore.Constants.LocalVariable;
+
+public class WildcardConstant extends LocalVariable {
+	public WildcardConstant(String value) {
+		super(value);
+	}
+}
