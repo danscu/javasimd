3916795a912df4b5a42c93c337f87572c24da521
Danke Xie <d.xie@sta.samsung.com>
2013-06-10 13:15:56 -0700

    Make 4 test-cases slower

--- test/paper/VectorTest.java
+++ test/paper/VectorTest.java
@@ -20,8 +20,11 @@ public class VectorTest {
 
     private int testArraySum(int[] ia) {
       int sum = 0;
-      for (int i = 0; i < ia.length; i++)
-         sum += ia[i];
+      for (int redundant = 0; redundant < 1000; redundant++) { 
+	   sum = 0;
+	   for (int i = 0; i < ia.length; i++)
+		 sum += ia[i];
+      }
       return sum;
     }
 
@@ -36,40 +39,129 @@ public class VectorTest {
 	}
     }
 
-    void testSLPVectorize(int a1, int a2, int b1, int b2, int A[]) {
-        A[0] = a1*(a1 + b1)/b1 + 50*b1/a1;
-        A[1] = a2*(a2 + b2)/b2 + 50*b2/a2;
+    void testSLPVectorize(int a1, int a2, int b1, int b2, int A[], int B[]) {
+	int i;
+	int r;
+	for (r = 0; r < 100; r++)
+	for (i = 0; i < A.length; i++) {
+        	A[i] = a1*(a1 + b1)/b1 + 50*b1/a1;
+        	B[i] = a2*(a2 + b2)/b2 + 50*b2/a2;
+	}
+    }
+
+    private void testNRMSort(int a[]) {
+	if(a.length < 2)
+		return;
+	int step = 1;
+	int startL, startR;
+	while(step < a.length) {
+		startL = 0;
+		startR = step;
+		while(startR + step <= a.length) {
+			//merge
+			int right[] = new int[(startR + step) - startR + 1];
+			int left[] = new int[(startL + step) - startL - 1];
+			for(int i = 0, k = startR; i < (right.length - 1); ++i, ++k) {
+				right[i] = a[k];
+			}
+			for(int i = 0, k = startL; i < (left.length - 1); ++i, ++k) {
+				left[i] = a[k];
+			}
+			right[right.length-1] = Integer.MAX_VALUE;
+			left[left.length-1] = Integer.MAX_VALUE;
+			for(int k = startL, m = 0, n = 0; k < (startR + step); ++k) {
+				if(left[m] <= right[n]) {
+					a[k] = left[m];
+					m++;
+				}
+				else {
+					a[k] = right[n];
+					n++;
+				}
+			}
+
+			startL = startR + step;
+			startR = startL + step;
+		}
+		if(startR < a.length) {
+			//merge
+			int right[] = new int[(startR + step) - startR + 1];
+			int left[] = new int[(startL + step) - startL - 1];
+			for(int i = 0, k = startR; i < (right.length - 1); ++i, ++k) {
+				right[i] = a[k];
+			}
+			for(int i = 0, k = startL; i < (left.length - 1); ++i, ++k) {
+				left[i] = a[k];
+			}
+			right[right.length-1] = Integer.MAX_VALUE;
+			left[left.length-1] = Integer.MAX_VALUE;
+			for(int k = startL, m = 0, n = 0; k < (startR + step); ++k) {
+				if(left[m] <= right[n]) {
+					a[k] = left[m];
+					m++;
+				}
+				else {
+					a[k] = right[n];
+					n++;
+				}
+			}
+		}
+		step *= 2;
+	}
+    }
+
+    private void testInsertionSort(int a[]) {
+		int i, j, key;
+		for(j = 1; j < a.length; j++) {
+			key = a[j];
+			for(i = j-1; (i >= 0) && (a[i] < key); i--) {
+				a[i+1] = a[i];
+			}
+			a[i+1] = key;
+		}
     }
 
     public static void main(String[] args) {
 	VectorTest inst = new VectorTest();
+	long startMs;
+	long durationMs;
+	int res;
 
-	// Test 1	
-	int res = inst.testSum(100);
-	System.out.println("Sum = " + res); 
-
-	// Test 2
-	int[] ia = new int[100];
-	for (int i = 0; i < 100; i++)
+	int[] ia = new int[1000000];
+	int[] ib = new int[1000000];
+	for (int i = 0; i < 1000000; i++) {
 		ia[i] = i;
+		ib[i] = i * 2;
+	}
+
+	// Test 1 array sum
+	startMs = System.currentTimeMillis();	
 	res = inst.testArraySum(ia);
+	durationMs = System.currentTimeMillis() - startMs;
+
 	System.out.println("Array sum = " + res); 
+	System.out.println(String.format("Array sum time: %d ms", durationMs));
+	System.err.println(durationMs);
 
-	// Test 3
-	inst.testSLPVectorize(10, 20, 30, 40, ia);
+	// Test 2 SLP
+	startMs = System.currentTimeMillis();
+	inst.testSLPVectorize(10, 20, 30, 40, ia, ib);
+	durationMs = System.currentTimeMillis() - startMs;
 	System.out.println("Straight line program result = " + ia[0] + " " +
-		ia[1]); 
+		ia[1] + " " + ib[0] + " " + ib[1]); 
+	System.out.println(String.format("SLP time: %d ms", durationMs));
+	System.err.println(durationMs);
 
-	// Test 4
+	// Test 3 -- selection sort
 	int N = 40000;
 	Random r = new Random(0);
 	ia = new int[N];
 	for (int i = 0; i < N; i++)
 		ia[i] = r.nextInt();
 
-	long startMs = System.currentTimeMillis();
+	startMs = System.currentTimeMillis();
 	inst.testSort(ia);
-	long durationMs = System.currentTimeMillis() - startMs;
+	durationMs = System.currentTimeMillis() - startMs;
 	System.out.println(String.format("Sort time: %d ms", durationMs));
 	System.err.println(durationMs);
 
@@ -80,7 +172,18 @@ public class VectorTest {
 		System.out.println();
 	}
 
-	System.out.println("Sorting done, N = " + N);
+	System.out.println("Selection sorting done, N = " + N);
+
+	// Test 4 -- insertion sort
+	r = new Random(0);
+	ia = new int[N];
+	for (int i = 0; i < N; i++)
+		ia[i] = r.nextInt();
+	startMs = System.currentTimeMillis();
+	inst.testSort(ia);
+	durationMs = System.currentTimeMillis() - startMs;
+	System.out.println(String.format("Insertion sort time: %d ms", durationMs));
+	System.err.println(durationMs);
     }
 }
 
