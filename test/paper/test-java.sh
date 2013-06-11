7c9d732f6bbd9ec46dab614f2e6ffa10f4daae5c
Danke Xie <d.xie@sta.samsung.com>
2013-06-11 13:13:46 -0700

    Add print for test.sh

--- test/paper/test-java.sh
+++ test/paper/test-java.sh
@@ -2,11 +2,13 @@ FILE="$1"
 LOG=log.txt
 
 javac $FILE.java
+echo "Java with JIT..."
 echo "Java with JIT: " >> $LOG
 java $FILE 2>> $LOG
 
 echo >> $LOG
 
+echo "Java without JIT... (about 1:20)"
 echo "Java without JIT: " >> $LOG
 if [ "$2" != "jit-only" ]; then
   java -Djava.compiler=NONE $FILE 2>> $LOG
